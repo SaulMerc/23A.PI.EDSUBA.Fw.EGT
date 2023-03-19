ae11e1b4a655d64bf1dc90ad81b742eb47270df8
SaulMerc <saul.mercado9192@alumnos.udg.mx>
2023-03-19 11:39:24 -0600

    Animacion typed y botones de cards

---
+++ assets/scripts/animations.js
@@ -0,0 +1,9 @@
+let typedBackspace = new Typed(".txt_typed",{
+    strings: ['EDSUBA EGT '],
+    typeSpeed: 200,
+    smartBackspace: true,
+    cursorChar: ' < />',
+    startDelay: 1000,
+    backDelay: 10000,
+    loop: true
+});
