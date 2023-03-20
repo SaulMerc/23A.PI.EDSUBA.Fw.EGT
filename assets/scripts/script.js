ca783bd03ff70ec23b9f62f721ef47436930d1f1
SaulMerc <saul.mercado9192@alumnos.udg.mx>
2023-03-20 17:09:49 -0600

    Correcciones e implementacion de codigo para el fr

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -193,3 +193,23 @@ function setResponsiveImages() {
 
 // Ejecuta la función al cargar la página
 setResponsiveImages();
+
+//
+$(document).ready(function(){
+
+  $("a").on('click', function(event) {
+
+    if (this.hash !== "") {
+
+      event.preventDefault();
+
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 890, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
+});
