280da6138020f092a8ed94bdf56874aed29dde6b
SaulMerc <saul.mercado9192@alumnos.udg.mx>
2023-03-20 18:32:59 -0600

    Elementos faltantes que no estaban en el index

--- assets/scripts/animations.js
+++ assets/scripts/animations.js
@@ -7,3 +7,26 @@ let typedBackspace = new Typed(".txt_typed",{
     backDelay: 10000,
     loop: true
 });
+
+/**
+ * Subida al cielo
+ */
+$(document).ready(function(){
+    const arriba = document.querySelector('#go-up');
+        
+    
+            $('#ir_Arriba').click(function(){
+            $('body, html').animate({
+                scrollTop: '0px'
+            }, 300);
+        });
+    
+        $(window).scroll(function(){
+            if( $(this).scrollTop() > 0 ){
+                $('#ir_Arriba').slideDown(300);
+            } else {
+                $('#ir_Arriba').slideUp(300);
+            }
+        });
+    
+    });
